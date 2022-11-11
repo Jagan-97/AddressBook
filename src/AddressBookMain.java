33cb685e62b62607320375e1bc646f1dbb8653a4
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-11 16:04:52 +0530

    Added add_contact logic

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public static void main(String args[]){
+    private static Scanner sc = new Scanner(System.in);
+    ArrayList addressBook;
+    AddressBookMain() {
+        addressBook = new ArrayList<>();
+    }
+    public static void main(String args[]) {
         System.out.println("Welcome to Address Book");
-        ContactPerson person = new ContactPerson();
+        boolean isExit = false;
+        AddressBookMain addressBook = new AddressBookMain();
+        while (!isExit) {
+            System.out.println("Enter Option" +
+                    "\n1. Add Person" +
+                    "\n2. Edit Person" +
+                    "\n3. Delete Person" +
+                    "\n4. Show Address Book" +
+                    "\n5. Exit");
+            int option = Integer.parseInt(sc.nextLine());
+            switch (option) {
+                case 1:
+                    addressBook.addPerson();
+                    break;
+            }
+
+        }
+    }
+
+    private void addPerson() {
+        System.out.println("Enter First name");
+        String firstName = sc.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName = sc.nextLine();
+        System.out.println("Enter Street");
+        String street = sc.nextLine();
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+        System.out.println("Enter Phone Number");
+        long mobileNumber=Long.parseLong(sc.nextLine());
+        //parameterized constructor calling of Contact person class
+        ContactPerson person = new ContactPerson(firstName, lastName, street, city, state, mobileNumber);
+        //Adding object element in arrayList
+        addressBook.add(person);
+
     }
 }
--- src/ContactPerson.java
+++ src/ContactPerson.java
@@ -6,15 +6,26 @@ public class ContactPerson {
     private String state;
     private long mobileNumber;
 
-    public void ContactPerson() {
+    public ContactPerson() {
     }
 
-    public void ContactPerson(String firstName, String lastName, String street, String city, String state, long mobileNumber) {
+    public ContactPerson(String firstName, String lastName, String street, String city, String state, long mobileNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
         this.city = city;
         this.state = state;
         this.mobileNumber = mobileNumber;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "FirstName='" + firstName + '\'' +
+                ",LastName='" + lastName + '\'' +
+                ",Street='" + street + '\'' +
+                ",City='" + city + '\'' +
+                ",State='" + state + '\'' +
+                ",MobileNumber'" + mobileNumber + '\'' +
+                '}';
     }
 }
